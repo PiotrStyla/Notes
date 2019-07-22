7f3b934657dd74be40ab23db5e9ccf26985572c0
Admin <p.styla@gmail.com>
2019-07-22 20:33:16 +0200

    2

--- app/src/main/java/pl/aplikacje/notes/NotesListActivity.java
+++ app/src/main/java/pl/aplikacje/notes/NotesListActivity.java
@@ -2,16 +2,59 @@ package pl.aplikacje.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import pl.aplikacje.notes.adapters.NotesRecyclerAdapter;
 import pl.aplikacje.notes.models.Note;
+import pl.aplikacje.notes.util.VerticalSpacingItemDecorator;
 
 public class NotesListActivity extends AppCompatActivity {
+
+    //UI components section
+    private RecyclerView mRecyclerView;
+
+
+    //vars secton
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notes_list);
+        mRecyclerView = findViewById(R.id.recyclerView);
 
-        Note note = new Note();
+        initRecyclerView();
+        insertFakeNotes();
     }
+
+    private void insertFakeNotes(){
+        for (int i = 0; i <1000 ; i++) {
+            Note note = new Note();
+            note.setTitle("Title #" + i);
+            note.setContent("Content #" + i);
+            note.setTimeStamp("Jul 2019");
+            mNotes.add(note);
+        }
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+
+
+    }
+
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNotesRecyclerAdapter);
+
+    }
+
+
 }
