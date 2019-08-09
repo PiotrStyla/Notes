2f6bd70aee6a833717e0ed5ce9d4198cfaa876d6
Admin <p.styla@gmail.com>
2019-08-09 06:37:55 +0200

    swiping notes frm the list

--- app/src/main/java/pl/aplikacje/notes/NotesListActivity.java
+++ app/src/main/java/pl/aplikacje/notes/NotesListActivity.java
@@ -1,11 +1,13 @@
 package pl.aplikacje.notes;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 
@@ -64,6 +66,7 @@ public class NotesListActivity extends AppCompatActivity
         mRecyclerView.setLayoutManager(linearLayoutManager);
         VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
         mRecyclerView.addItemDecoration(itemDecorator);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(mRecyclerView);
         mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes,this);
         mRecyclerView.setAdapter(mNotesRecyclerAdapter);
 
@@ -84,4 +87,23 @@ public class NotesListActivity extends AppCompatActivity
         Intent intent = new Intent(this, NoteActivity.class);
         startActivity(intent);
     }
+
+    private void deleteNote(Note note){
+        mNotes.remove(note);
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT) {
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            deleteNote(mNotes.get(viewHolder.getAdapterPosition()));
+
+        }
+    };
 }
