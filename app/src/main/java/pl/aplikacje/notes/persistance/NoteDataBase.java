7f92f2b0cdaa82657889bb98082604de0d9d4314
Admin <p.styla@gmail.com>
2019-08-17 17:10:14 +0200

    data base

---
+++ app/src/main/java/pl/aplikacje/notes/persistance/NoteDataBase.java
@@ -0,0 +1,30 @@
+package pl.aplikacje.notes.persistance;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import pl.aplikacje.notes.models.Note;
+
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDataBase extends RoomDatabase {
+
+
+    public static final String DATABASE_NAME = "notes_db";
+
+    private static NoteDataBase instance;
+
+    static NoteDataBase getInstance(final Context context) {
+        if(instance == null){
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    NoteDataBase.class,
+                    DATABASE_NAME
+            ).build();
+        }
+        return instance;
+    }
+}
